c22f9528978fe8a2336e627ad1f9356de6eef5e7
Tai Nguyen <tai.nguyent@s3corp.com.vn>
2025-10-24 17:07:44 +0700

    Added simple AntDesign table in ProductList

--- Exercise3/FormValidation/UserManagement/src/components/layout/ProductList.jsx
+++ Exercise3/FormValidation/UserManagement/src/components/layout/ProductList.jsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
+import { mockProductColumns, mockProducts } from "../../data/mockData";
 import DashboardHeader from "../common/DashboardHeader";
-
+import { Table } from "antd";
 const ProductList = () => {
+  const [pagination, setPagination] = useState({
+    current: 1,
+    pageSize: 10,
+    showSizeChanger: true,
+    pageSizeOptions: [5, 10, 20, 50],
+  });
+
+  const handleTableChange = (newPagination) => {
+    setPagination({
+      ...pagination,
+      current: newPagination.current,
+      pageSize: newPagination.pageSize,
+    });
+  };
   return (
     <div className="panel">
-      <DashboardHeader title={`Danh sách sản phẩm`}/>
-      <p>lorem ipsum.</p>
+      <DashboardHeader title={`Danh sách sản phẩm`} />
+
+      <Table
+        columns={mockProductColumns}
+        dataSource={mockProducts}
+        rowKey="productId"
+        pagination={pagination}
+        bordered
+        scroll={{ x: 900 }}
+        onChange={handleTableChange}
+      />
     </div>
   );
 };
